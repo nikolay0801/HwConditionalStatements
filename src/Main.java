2dbc3c8213557a3a63b019c07c86d968849d851d
dryg <xboxseriaskell@gmail.com>
2023-02-24 11:29:05 +0500

    Условные переменные

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int ears = 18;
+        System.out.println("Если возраст человека равен 18, то он совершеннолетний");
+        int earsI = 17;
+        if ( earsI >= 18) {
+            System.out.println("Если возраст человека больше 18, то он совершеннолетний");
+        } else
+    System.out.println("Если возраст человека равен 17, то он несовершеннолетний");}
+}
+
+
+
+
+
+
+
+
